84c1f6e865e6682d07c60f0207266b4c4f07e472
Andrea Vecino <avecino@picap.co>
2019-07-17 13:54:40 -0500

    [fix]: numero de reservas de una pelicula por dia

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :reservations do
-    get :reservations
-  end
+  resources :reservations
+  get '/numbers_reservations' => 'reservations#reservations'
   resources :movies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
